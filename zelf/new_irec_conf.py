730273b5348aa1990e706e8a847bf656f06565fc
stenstokroos <stenstokroos@gmail.com>
2024-07-03 16:57:43 +0200

    new_irec

---
+++ zelf/new_irec_conf.py
@@ -0,0 +1,300 @@
+import tensorflow as tf
+import time
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from scipy.sparse import csr_matrix, coo_matrix
+from tqdm import tqdm
+import os
+import math
+import argparse
+
+# Set directory paths and random seed
+dir_ml = 'C:/Users/Sten Stokroos/Desktop/NEW/zelf/Data/out'
+randseed = 42
+
+def choose_data(dat, test_size=0.1):
+    if dat == 'r3':
+        train = pd.read_csv(os.path.join(dir_r3, 'r3_train.csv'), sep="\t", header=None, names=['userId', 'songId', 'rating'], usecols=[0, 1, 2], engine="python")
+        test = pd.read_csv(os.path.join(dir_r3, 'r3_test.csv'), sep="\t", header=None, names=['userId', 'songId', 'rating'], usecols=[0, 1, 2], engine="python")
+        
+        # Combine train and test to create the full dataset
+        r3_full = pd.concat([train, test]).sort_values(by=['userId', 'songId']).reset_index(drop=True)
+        
+        return r3_full, train, test
+    elif dat == 'ml':
+        ml_full = pd.read_csv(os.path.join(dir_ml, 'ml-1m_full.csv'), sep="\t", header=None, names=['userId', 'songId', 'rating'], usecols=[0, 1, 2], engine="python")
+        train, test = train_test_split(ml_full, test_size=test_size, random_state=randseed)
+        return ml_full, train, test
+    else:
+        print('Wrong data input')
+        return None, None, None
+
+class IAutoRec():
+    def __init__(self, sess, args, num_users, num_items, R, mask_R, C, train_R, train_mask_R, test_R, test_mask_R, 
+                 confounder_R, num_train_ratings, num_test_ratings, user_train_set, item_train_set, 
+                 user_test_set, item_test_set, result_path):
+        self.sess = sess
+        self.args = args
+
+        self.num_users = num_users
+        self.num_items = num_items
+
+        self.R = R
+        self.mask_R = mask_R
+        self.C = C
+        self.train_R = train_R
+        self.train_mask_R = train_mask_R
+        self.test_R = test_R
+        self.test_mask_R = test_mask_R
+        self.confounder_R = confounder_R
+        self.num_train_ratings = num_train_ratings
+        self.num_test_ratings = num_test_ratings
+
+        self.user_train_set = user_train_set
+        self.item_train_set = item_train_set
+        self.user_test_set = user_test_set
+        self.item_test_set = item_test_set
+
+        self.hidden_neuron = args.hidden_neuron
+        self.train_epoch = args.train_epoch
+        self.batch_size = args.batch_size
+        self.num_batch = int(math.ceil(self.num_users / float(self.batch_size)))
+
+        self.base_lr = args.base_lr
+        self.optimizer_method = args.optimizer_method
+        self.display_step = args.display_step
+        self.random_seed = args.random_seed
+
+        self.global_step = tf.Variable(0, trainable=False)
+        self.decay_epoch_step = args.decay_epoch_step
+        self.decay_step = self.decay_epoch_step * self.num_batch
+        self.lr = tf.compat.v1.train.exponential_decay(self.base_lr, self.global_step, self.decay_step, 0.96, staircase=True)
+        self.lambda_value = args.lambda_value
+
+        self.train_cost_list = []
+        self.test_cost_list = []
+        self.test_rmse_list = []
+
+        self.result_path = result_path
+        self.grad_clip = args.grad_clip
+
+    def run(self):
+        self.prepare_model()
+        init = tf.compat.v1.global_variables_initializer()
+        self.sess.run(init)
+        for epoch_itr in range(self.train_epoch):
+            self.train_model(epoch_itr)
+            self.test_model(epoch_itr)
+        self.make_records()
+
+    def prepare_model(self):
+        self.input_R = tf.compat.v1.placeholder(dtype=tf.float32, shape=[None, self.num_items], name="input_R")
+        self.input_mask_R = tf.compat.v1.placeholder(dtype=tf.float32, shape=[None, self.num_items], name="input_mask_R")
+        self.input_confounder_R = tf.compat.v1.placeholder(dtype=tf.float32, shape=[None, self.num_items], name="input_confounder_R")
+
+        # Rating path
+        V_R = tf.compat.v1.get_variable(name="V_R", initializer=tf.random.truncated_normal(shape=[self.num_items, self.hidden_neuron], mean=0, stddev=0.03), dtype=tf.float32)
+        W_R = tf.compat.v1.get_variable(name="W_R", initializer=tf.random.truncated_normal(shape=[self.hidden_neuron, self.num_items], mean=0, stddev=0.03), dtype=tf.float32)
+        mu_R = tf.compat.v1.get_variable(name="mu_R", initializer=tf.zeros(shape=self.hidden_neuron), dtype=tf.float32)
+        b_R = tf.compat.v1.get_variable(name="b_R", initializer=tf.zeros(shape=self.num_items), dtype=tf.float32)
+
+        pre_Encoder_R = tf.matmul(self.input_R, V_R) + mu_R
+        Encoder_R = tf.nn.sigmoid(pre_Encoder_R)
+        pre_Decoder_R = tf.matmul(Encoder_R, W_R) + b_R
+
+        # Confounder path
+        V_C = tf.compat.v1.get_variable(name="V_C", initializer=tf.random.truncated_normal(shape=[self.num_items, self.hidden_neuron], mean=0, stddev=0.03), dtype=tf.float32)
+        W_C = tf.compat.v1.get_variable(name="W_C", initializer=tf.random.truncated_normal(shape=[self.hidden_neuron, self.num_items], mean=0, stddev=0.03), dtype=tf.float32)
+        mu_C = tf.compat.v1.get_variable(name="mu_C", initializer=tf.zeros(shape=self.hidden_neuron), dtype=tf.float32)
+        b_C = tf.compat.v1.get_variable(name="b_C", initializer=tf.zeros(shape=self.num_items), dtype=tf.float32)
+
+        pre_Encoder_C = tf.matmul(self.input_confounder_R, V_C) + mu_C
+        Encoder_C = tf.nn.sigmoid(pre_Encoder_C)
+        pre_Decoder_C = tf.matmul(Encoder_C, W_C) + b_C
+
+        # Combine paths
+        pre_Decoder = pre_Decoder_R + pre_Decoder_C
+        self.Decoder = tf.identity(pre_Decoder)
+
+        # Loss calculation
+        pre_rec_cost = tf.multiply((self.input_R - self.Decoder), self.input_mask_R)
+        rec_cost = tf.square(self.l2_norm(pre_rec_cost))
+        pre_reg_cost = tf.square(self.l2_norm(W_R)) + tf.square(self.l2_norm(V_R)) + tf.square(self.l2_norm(W_C)) + tf.square(self.l2_norm(V_C))
+        reg_cost = self.lambda_value * 0.5 * pre_reg_cost
+
+        self.cost = rec_cost + reg_cost
+
+        if self.optimizer_method == "Adam":
+            optimizer = tf.compat.v1.train.AdamOptimizer(self.lr)
+        elif self.optimizer_method == "RMSProp":
+            optimizer = tf.compat.v1.train.RMSPropOptimizer(self.lr)
+        else:
+            raise ValueError("Optimizer Key ERROR")
+
+        if self.grad_clip:
+            gvs = optimizer.compute_gradients(self.cost)
+            capped_gvs = [(tf.clip_by_value(grad, -5., 5.), var) for grad, var in gvs]
+            self.optimizer = optimizer.apply_gradients(capped_gvs, global_step=self.global_step)
+        else:
+            self.optimizer = optimizer.minimize(self.cost, global_step=self.global_step)
+
+    def train_model(self, itr):
+        start_time = time.time()
+        random_perm_doc_idx = np.random.permutation(self.num_users)
+
+        batch_cost = 0
+        for i in range(self.num_batch):
+            if i == self.num_batch - 1:
+                batch_set_idx = random_perm_doc_idx[i * self.batch_size:]
+            elif i < self.num_batch - 1:
+                batch_set_idx = random_perm_doc_idx[i * self.batch_size: (i + 1) * self.batch_size]
+
+            _, Cost = self.sess.run(
+                [self.optimizer, self.cost],
+                feed_dict={self.input_R: self.train_R[batch_set_idx, :],
+                           self.input_mask_R: self.train_mask_R[batch_set_idx, :],
+                           self.input_confounder_R: self.confounder_R[batch_set_idx, :]})
+
+            batch_cost = batch_cost + Cost
+        self.train_cost_list.append(batch_cost)
+
+        if (itr + 1) % self.display_step == 0:
+            print("Training //", "Epoch %d //" % (itr), " Total cost = {:.2f}".format(batch_cost),
+                  "Elapsed time : %d sec" % (time.time() - start_time))
+
+    def test_model(self, itr):
+        start_time = time.time()
+        Cost, Decoder = self.sess.run(
+            [self.cost, self.Decoder],
+            feed_dict={self.input_R: self.test_R,
+                       self.input_mask_R: self.test_mask_R,
+                       self.input_confounder_R: self.confounder_R})
+
+        self.test_cost_list.append(Cost)
+
+        if (itr + 1) % self.display_step == 0:
+            Estimated_R = Decoder.clip(min=1, max=5)
+            unseen_user_test_list = list(self.user_test_set - self.user_train_set)
+            unseen_item_test_list = list(self.item_test_set - self.item_train_set)
+
+            for user in unseen_user_test_list:
+                for item in unseen_item_test_list:
+                    if self.test_mask_R[user, item] == 1:  # exist in test set
+                        Estimated_R[user, item] = 3
+
+            pre_numerator = np.multiply((Estimated_R - self.test_R), self.test_mask_R)
+            numerator = np.sum(np.square(pre_numerator))
+            denominator = self.num_test_ratings
+            RMSE = np.sqrt(numerator / float(denominator))
+
+            self.test_rmse_list.append(RMSE)
+
+            print("Testing //", "Epoch %d //" % (itr), " Total cost = {:.2f}".format(Cost), " RMSE = {:.5f}".format(RMSE),
+                  "Elapsed time : %d sec" % (time.time() - start_time))
+            print("=" * 100)
+
+    def make_records(self):
+        if not os.path.exists(self.result_path):
+            os.makedirs(self.result_path)
+
+        basic_info = self.result_path + "basic_info.txt"
+        train_record = self.result_path + "train_record.txt"
+        test_record = self.result_path + "test_record.txt"
+
+        with open(train_record, 'w') as f:
+            f.write(str("Cost:"))
+            f.write('\t')
+            for itr in range(len(self.train_cost_list)):
+                f.write(str(self.train_cost_list[itr]))
+                f.write('\t')
+            f.write('\n')
+
+        with open(test_record, 'w') as g:
+            g.write(str("Cost:"))
+            g.write('\t')
+            for itr in range(len(self.test_cost_list)):
+                g.write(str(self.test_cost_list[itr]))
+                g.write('\t')
+            g.write('\n')
+
+            g.write(str("RMSE:"))
+            for itr in range(len(self.test_rmse_list)):
+                g.write(str(self.test_rmse_list[itr]))
+                g.write('\t')
+            g.write('\n')
+
+        with open(basic_info, 'w') as h:
+            h.write(str(self.args))
+
+    def l2_norm(self, tensor):
+        return tf.sqrt(tf.reduce_sum(tf.square(tensor)))
+
+def prepare_data(train, test, confounder, a, b):
+    num_users = max(train['userId'].max(), test['userId'].max()) + 1
+    num_items = max(train['songId'].max(), test['songId'].max()) + 1
+
+    R = np.zeros((num_users, num_items))
+    mask_R = np.zeros((num_users, num_items))
+    C = np.ones((num_users, num_items)) * b
+
+    train_R = np.zeros((num_users, num_items))
+    test_R = np.zeros((num_users, num_items))
+    confounder_R = np.zeros((num_users, num_items))
+
+    train_mask_R = np.zeros((num_users, num_items))
+    test_mask_R = np.zeros((num_users, num_items))
+
+    for line in train.itertuples():
+        user_idx = line[1]
+        item_idx = line[2]
+        rating = line[3]
+        R[user_idx, item_idx] = rating
+        mask_R[user_idx, item_idx] = 1
+        C[user_idx, item_idx] = a
+        train_R[user_idx, item_idx] = rating
+        train_mask_R[user_idx, item_idx] = 1
+
+    for line in test.itertuples():
+        user_idx = line[1]
+        item_idx = line[2]
+        rating = line[3]
+        test_R[user_idx, item_idx] = rating
+        test_mask_R[user_idx, item_idx] = 1
+
+
+    for line in confounder.itertuples():
+        user_idx = int(line[1])
+        item_idx = int(line[2])
+        confounder_val = line[3]
+        confounder_R[user_idx, item_idx] = confounder_val
+        
+
+    user_train_set = set(train['userId'])
+    item_train_set = set(train['songId'])
+    user_test_set = set(test['userId'])
+    item_test_set = set(test['songId'])
+
+    num_train_ratings = len(train)
+    num_test_ratings = len(test)
+
+    return R, mask_R, C, train_R, train_mask_R, test_R, test_mask_R, confounder_R, num_train_ratings, num_test_ratings, \
+           user_train_set, item_train_set, user_test_set, item_test_set, num_users, num_items
+
+# Load and prepare data
+ml_full, train, test = choose_data('ml', test_size=0.2)
+confounder = pd.read_csv('C:/Users/Sten Stokroos/Desktop/NEW/zelf/Data/exposure_output/exp_ml_0.1_k30.csv', header=None)  # Update path accordingly
+a = 1  # Adjust these values as needed
+b = 0  # Adjust these values as needed
+R, mask_R, C, train_R, train_mask_R, test_R, test_mask_R, confounder_R, num_train_ratings, num_test_ratings, \
+user_train_set, item_train_set, user_test_set, item_test_set, num_users, num_items = prepare_data(train, test, confounder, a, b)
+
+# Set TensorFlow session
+config = tf.compat.v1.ConfigProto()
+config.gpu_options.allow_growth = True
+
+args = argparse.Namespace(hidden_neuron=500, lambda_value=1, train_epoch=20, batch_size=100, optimizer_method='Adam', grad_clip=False, base_lr=1e-3, decay_epoch_step=50, random_seed=1000, display_step=1)
+
+with tf.compat.v1.Session(config=config) as sess:
+    model = IAutoRec(sess, args, num_users, num_items, R, mask_R, C, train_R, train_mask_R, test_R, test_mask_R, confounder_R, num_train_ratings, num_test_ratings, user_train_set, item_train_set, user_test_set, item_test_set, 'results/')
+    model.run()
